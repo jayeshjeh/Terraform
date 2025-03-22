75e861a0b7a092bd9fd7098eb95f36980a6ed679
jayeshjeh <jay.parmar.11169@gmail.com>
2025-03-22 14:54:57 +0530

    created env

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 #This Terraform Code Deploys Basic VPC Infra.
 provider "aws" {
-    region = "${var.aws_region}"
+  region = var.aws_region
 }
 
 terraform {
@@ -13,75 +13,75 @@ terraform {
 
 
 resource "aws_vpc" "default" {
-    cidr_block = "${var.vpc_cidr}"
-    enable_dns_hostnames = true
-    tags = {
-        Name = "${var.vpc_name}"
-	Owner = "Saikiran"
-    }
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  tags = {
+    Name  = "${var.vpc_name}"
+    Owner = "Saikiran"
+  }
 }
 
 resource "aws_internet_gateway" "default" {
-    vpc_id = "${aws_vpc.default.id}"
-	tags = {
-        Name = "${var.IGW_name}"
-    }
+  vpc_id = aws_vpc.default.id
+  tags = {
+    Name = "${var.IGW_name}"
+  }
 }
 
 resource "aws_subnet" "subnet1-public" {
-    vpc_id = "${aws_vpc.default.id}"
-    cidr_block = "${var.public_subnet1_cidr}"
-    availability_zone = "us-east-1a"
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = var.public_subnet1_cidr
+  availability_zone = "us-east-1a"
 
-    tags = {
-        Name = "${var.public_subnet1_name}"
-    }
+  tags = {
+    Name = "${var.public_subnet1_name}"
+  }
 }
 
 resource "aws_subnet" "subnet2-public" {
-    vpc_id = "${aws_vpc.default.id}"
-    cidr_block = "${var.public_subnet2_cidr}"
-    availability_zone = "us-east-1b"
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = var.public_subnet2_cidr
+  availability_zone = "us-east-1b"
 
-    tags = {
-        Name = "${var.public_subnet2_name}"
-    }
+  tags = {
+    Name = "${var.public_subnet2_name}"
+  }
 }
 
 resource "aws_subnet" "subnet3-public" {
-    vpc_id = "${aws_vpc.default.id}"
-    cidr_block = "${var.public_subnet3_cidr}"
-    availability_zone = "us-east-1c"
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = var.public_subnet3_cidr
+  availability_zone = "us-east-1c"
 
-    tags = {
-        Name = "${var.public_subnet3_name}"
-    }
-	
+  tags = {
+    Name = "${var.public_subnet3_name}"
+  }
+
 }
 
 
 resource "aws_route_table" "terraform-public" {
-    vpc_id = "${aws_vpc.default.id}"
+  vpc_id = aws_vpc.default.id
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.default.id}"
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.default.id
+  }
 
-    tags = {
-        Name = "${var.Main_Routing_Table}"
-    }
+  tags = {
+    Name = "${var.Main_Routing_Table}"
+  }
 }
 
 resource "aws_route_table_association" "terraform-public" {
-    subnet_id = "${aws_subnet.subnet1-public.id}"
-    route_table_id = "${aws_route_table.terraform-public.id}"
+  subnet_id      = aws_subnet.subnet1-public.id
+  route_table_id = aws_route_table.terraform-public.id
 }
 
 resource "aws_security_group" "allow_all" {
   name        = "allow_all"
   description = "Allow all inbound traffic"
-  vpc_id      = "${aws_vpc.default.id}"
+  vpc_id      = aws_vpc.default.id
 
   ingress {
     from_port   = 0
@@ -91,11 +91,11 @@ resource "aws_security_group" "allow_all" {
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-    }
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 # data "aws_ami" "my_ami" {
