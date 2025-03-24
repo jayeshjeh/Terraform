dba81c7258444f84e8369399018ba0530d1dae5c
jayeshjeh <jay.parmar.11169@gmail.com>
2025-03-24 11:13:54 +0530

    fmt
    Signed-off-by: jayeshjeh <jay.parmar.11169@gmail.com>

--- Functions/main.tf
+++ Functions/main.tf
@@ -114,7 +114,7 @@ resource "aws_route_table_association" "private-subnet-RT-association" {
 }
 
 resource "aws_security_group" "allow_all" {
-  name        = "allow_all"
+  name        = "${var.vpc_name}-SG"
   description = "Allow all inbound traffic"
   vpc_id      = aws_vpc.default.id
 
