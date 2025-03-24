40bc49f4cd5e96335dfd09e389a66fa7839d0c3e
jayeshjeh <jay.parmar.11169@gmail.com>
2025-03-24 11:32:40 +0530

    fmt
    Signed-off-by: jayeshjeh <jay.parmar.11169@gmail.com>

--- Functions/main.tf
+++ Functions/main.tf
@@ -118,11 +118,15 @@ resource "aws_security_group" "allow_all" {
   description = "Allow all inbound traffic"
   vpc_id      = aws_vpc.default.id
 
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = var.ingress_ports
+    content {
+      from_port = ingress.value
+      to_port   = ingress.value
+      protocol  = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    
   }
 
   egress {
