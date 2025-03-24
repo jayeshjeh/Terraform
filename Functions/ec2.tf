d09939cc07b02efd62c4241ea56bc002f4e34df1
jayeshjeh <jay.parmar.11169@gmail.com>
2025-03-24 18:51:55 +0530

    lookup for amis
    Signed-off-by: jayeshjeh <jay.parmar.11169@gmail.com>

--- Functions/ec2.tf
+++ Functions/ec2.tf
@@ -1,26 +1,27 @@
 resource "aws_instance" "public-servers" {
-    count = length(var.public_cidr_block)
-    ami = lookup(var.amis, var.aws_region)
-    availability_zone = "us-east-1a"
-    instance_type = "t2.micro"
-    key_name = var.keyname
-    subnet_id = element(aws_subnet.public-subnets[*].id, count.index)
-    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
-    associate_public_ip_address = true	
+  count                       = length(var.public_cidr_block)
+  ami                         = lookup(var.amis, var.aws_region)
+  availability_zone           = "us-east-1a"
+  instance_type               = "t2.micro"
+  key_name                    = var.keyname
+  subnet_id                   = element(aws_subnet.public-subnets[*].id, count.index)
+  vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
+  associate_public_ip_address = true
 
-    tags = {
-        Name        = "${var.vpc_name}-public-server-${count.index + 1}"
-        Owner       = local.Owner
-        CostCenter  = local.CostCenter
-        TeamDL      = local.TeamDL
-        environment = "${var.environment}"
-    }
-    
-     user_data = <<-EOF
+  tags = {
+    Name        = "${var.vpc_name}-public-server-${count.index + 1}"
+    Owner       = local.Owner
+    CostCenter  = local.CostCenter
+    TeamDL      = local.TeamDL
+    environment = "${var.environment}"
+  }
+
+  user_data = <<-EOF
      #!/bin/bash
      	sudo apt-get update
      	sudo apt-get install -y nginx
      	echo "<h1>${var.environment}-Server-1</h1>" | sudo tee /var/www/html/index.html
+        echo "<h1>${var.vpc_name}-public-server-${count.index + 1}</h1>" | sudo tee /var/www/html/index.html
      	sudo systemctl start nginx
      	sudo systemctl enable nginx
      EOF
--- Functions/main.tf
+++ Functions/main.tf
@@ -121,12 +121,12 @@ resource "aws_security_group" "allow_all" {
   dynamic "ingress" {
     for_each = var.ingress_ports
     content {
-      from_port = ingress.value
-      to_port   = ingress.value
-      protocol  = "tcp"
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     }
-    
+
   }
 
   egress {
