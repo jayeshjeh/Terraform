4ab04696399bbbca3f69010693fff345d2375324
jayeshjeh <jay.parmar.11169@gmail.com>
2025-03-24 11:02:46 +0530

    fmt
    Signed-off-by: jayeshjeh <jay.parmar.11169@gmail.com>

--- Functions/main.tf
+++ Functions/main.tf
@@ -102,14 +102,14 @@ resource "aws_route_table_association" "public-subnet-RT-association" {
   count          = 3
   subnet_id      = element(aws_subnet.public-subnets[*].id, count.index)
   route_table_id = aws_route_table.public-RT.id
-  
+
 }
 
 resource "aws_route_table_association" "private-subnet-RT-association" {
   count          = 3
   subnet_id      = element(aws_subnet.private-subnets[*].id, count.index)
   route_table_id = aws_route_table.private-RT.id
-  
+
 }
 
 resource "aws_security_group" "allow_all" {
