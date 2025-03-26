de33c03b30d9e261308e60271d4626da9e165224
jayeshjeh <jay.parmar.11169@gmail.com>
2025-03-26 14:48:08 +0530

    correcting user-data.sh
    Signed-off-by: jayeshjeh <jay.parmar.11169@gmail.com>

--- Functions/user-data.sh
+++ Functions/user-data.sh
@@ -1,12 +1,10 @@
-  user_data = <<-EOF
-     #!/bin/bash
-     	sudo apt-get update
-     	sudo apt-get install -y nginx
-        sudo apt install git -y
-     	echo "<h1>${var.environment}-Server-1</h1>" | sudo tee /var/www/html/index.html
-        echo "<h1>${var.vpc_name}-private-server-${count.index + 1}</h1>" | sudo tee /var/www/html/index.html
-        echo "<h1> git --version </h1>" | sudo tee /var/www/html/index.html
-     	sudo systemctl start nginx
-     	sudo systemctl enable nginx
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y nginx
+sudo apt install git -y
+echo "<h1>${var.environment}-Server-1</h1>" | sudo tee /var/www/html/index.html
+echo "<h1>${var.vpc_name}-private-server-${count.index + 1}</h1>" | sudo tee -a /var/www/html/index.html
+git --version | sudo tee -a /var/www/html/index.html
+sudo systemctl start nginx
+sudo systemctl enable nginx
 
-     EOF
