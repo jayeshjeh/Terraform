d9803cd7bc7c18e472d6990f7b0897d8d5f84181
jayeshjeh <jay.parmar.11169@gmail.com>
2025-04-08 14:18:08 +0530

    sg typo

--- modules_practice/modules/sg/main.tf
+++ modules_practice/modules/sg/main.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "this" {
     for_each = var.ingress_ports
     content {
       from_port   = ingress.value
-      to_port     = ingreess.value
+      to_port     = ingress.value
       protocol    = "tcp"
       cidr_blocks = "0.0.0.0/0"
     }
