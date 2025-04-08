2471bf977d382312cff70a69de081ebf75d735d2
jayeshjeh <jay.parmar.11169@gmail.com>
2025-04-08 14:25:20 +0530

    sg egress reconfig
    Signed-off-by: jayeshjeh <jay.parmar.11169@gmail.com>

--- modules_practice/modules/sg/main.tf
+++ modules_practice/modules/sg/main.tf
@@ -9,21 +9,16 @@ resource "aws_security_group" "this" {
       from_port   = ingress.value
       to_port     = ingress.value
       protocol    = "tcp"
-      cidr_blocks = "0.0.0.0/0"
+      cidr_blocks = ["0.0.0.0/0"]
     }
 
   }
 
-  dynamic "egress" {
-
-    for_each = var.egress_ports
-    content {
-      from_port   = egress.value
-      to_port     = egress.value
-      protocol    = "tcp"
-      cidr_blocks = "0.0.0.0/0"
-    }
-
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
