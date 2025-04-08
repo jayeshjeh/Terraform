781aecb16cf62cd7b388c7225f37f05a24445215
jayeshjeh <jay.parmar.11169@gmail.com>
2025-04-08 15:23:04 +0530

    removing azs

--- modules_practice/modules/instances/main.tf
+++ modules_practice/modules/instances/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "public" {
   vpc_security_group_ids      = var.sg_id
   user_data                   = var.user_data
   associate_public_ip_address = true
-  availability_zone           = element(var.azs, count.index % length(var.azs))
+
 
   tags = {
     Name = "${var.env}-public-{count.index + 1}"
@@ -19,7 +19,6 @@ resource "aws_instance" "private" {
   instance_type          = var.instance_type
   subnet_id              = element(var.subnet_ids, count.index % length(var.subnet_ids))
   vpc_security_group_ids = var.sg_id
-  availability_zone      = element(var.azs, count.index % length(var.azs))
 
   tags = {
     Name = "${var.env}-private-{count.index + 1}"
