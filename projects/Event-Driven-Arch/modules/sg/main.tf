b72e137579570426c54c0a2cdeb02a0f686a65bc
jayeshjeh <jay.parmar.11169@gmail.com>
2025-05-05 10:42:09 +0530

    security group module

---
+++ projects/Event-Driven-Arch/modules/sg/main.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "this" {
+    vpc_id = var.vpc_id
+    name = "${var.env}-SG"
+
+    dynamic "ingress" {
+
+        for_each = var.ingress_ports
+        content {
+          from_port = ingress.value
+          to_port = ingress.value
+          protocol = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+      name = "${var.env}-SG"
+    }
+  
+}
